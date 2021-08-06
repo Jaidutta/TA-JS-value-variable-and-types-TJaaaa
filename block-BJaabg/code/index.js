5f38571d72ae86b89d0ca33d4d18ae53dbb8cec4
Jaidutta <dutta_joy@hotmail.com>
2021-08-06 01:06:37 +0100

    feat: completes Assignment: Level 3

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -16,3 +16,40 @@ var amount = 0;
 // your code goes here
 
 // ⛑ Answer of the above will `$334.76`.
+
+
+
+function calculateTax(amount) {
+  return amount * TAX_RATE;
+}
+
+function formatAmount(amount) {
+  return "$" + amount.toFixed( 2 );
+}
+
+// keep buying phones while you still have money
+while (amount < bank_balance) {
+  // buy a new phone!
+  amount = amount + PHONE_PRICE;
+
+  // can we afford the accessory?
+  if (amount < SPENDING_THRESHOLD) {
+      amount = amount + ACCESSORY_PRICE;
+  }
+}
+
+// don't forget to pay the government, too
+amount = amount + calculateTax( amount );
+
+console.log(
+  "Your purchase: " + formatAmount( amount )
+);
+// Your purchase: $334.76
+
+// can you actually afford this purchase?
+if (amount > bank_balance) {
+  console.log(
+      "You can't afford this purchase. :("
+  );
+}
+// You can't afford this purchase. :(
